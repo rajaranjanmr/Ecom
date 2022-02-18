b480b3f8c6bcc4ea817967d9a288bd8e0733c056
raranjan <raja.ranjan@genesys.com>
2022-02-19 03:09:29 +0530

    adding index page of Ecom website

---
+++ login/login.js
@@ -0,0 +1,14 @@
+const idEl = document.querySelector("#id02");
+const loginBtnEl = document.querySelector(".login-btn-ecom");
+const cancelBtnEl = document.querySelector(".cancel-btn")
+const closeEl = document.querySelector(".close-item");
+loginBtnEl.addEventListener("click",()=>{
+    idEl.style.display='block';
+    idEl.style.zIndex = '3';
+})
+cancelBtnEl.addEventListener("click",()=>{
+    idEl.style.display='none';
+})
+closeEl.addEventListener("click",()=>{
+    idEl.style.display='none';
+})
